3410417650179feb601babc2482e522258bf78aa
Shashwat Rakesh <shashwat.rakesh97@gmail.com>
2023-05-05 00:00:22 -0700

    episode-01

---
+++ App.js
@@ -0,0 +1,39 @@
+/**
+ *
+ * <div id="parent">
+ *      <div id="child1">
+ *              <h1 id="heading">Sibling 1</h1>
+ *              <h1 id="heading">Sibling 2</h1>
+ *      </div>
+ *      <div id="child2">
+ *              <h1 id="heading">Sibling 1</h1>
+ *              <h1 id="heading">Sibling 2</h1>
+ *      </div>
+ * </div>
+ */
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  [
+    React.createElement("div", { id: "child1" }, [
+      React.createElement("h1", { id: "heading" }, "Sibling 1"),
+      React.createElement("h1", { id: "heading" }, "Sibling 2"),
+    ]),
+  ],
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", { id: "heading" }, "Sibling 1"),
+    React.createElement("h1", { id: "heading" }, "Sibling 2"),
+  ])
+);
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World from React!"
+);
+
+console.log(parent); //object
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
