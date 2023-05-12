abd4c3f2cd8b59a671bbac0f820d6b8bed25ed41
Shashwat Rakesh <shashwat.rakesh97@gmail.com>
2023-05-12 03:24:30 -0700

    Episode-03

--- App.js
+++ App.js
@@ -15,6 +15,7 @@ import ReactDOM from "react-dom/client";
  * </div>
  */
 
+/* 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
     React.createElement("h1", { id: "heading" }, "Sibling 1"),
@@ -35,5 +36,49 @@ const heading = React.createElement(
 
 console.log(parent); //object
 
+*/
+
+//React Element
+/*
+const jsxHeading = (
+  <h1 className="heading">Hello World from React using JSX 🚀!</h1>
+);
+*/
+
+//console.log(jsxHeading); //object
+
+const elem = <span>React Element</span>;
+const titleElement = (
+  <h1 className="heading">{elem}Hello World from React Element 🚀!</h1> //element inside element
+);
+
+//React Functional Component
+const Title = () => {
+  return (
+    <h1 className="heading" tabIndex="5">
+      Hello World from React using JSX 🚀!
+    </h1>
+  );
+};
+
+// component inside component
+// element inside compmnent
+const HeadingComponent = () => (
+  <>
+    <div id="container">
+      <Title />
+      {Title()}
+      <Title></Title>
+      {titleElement}
+      <h1 className="heading">Namaste React Functional Component</h1>
+    </div>
+    <div id="container-2"></div>
+  </>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent />); // Now it becaomes an HTML element when we render it on the DOM.
+
+//using multiple roots
+// const root2 = ReactDOM.createRoot(document.getElementById("root2"));
+// root2.render(titleElement); // Now it becomes an HTML element when we render it on the DOM.
